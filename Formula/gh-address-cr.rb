75e55058719121976c90a5cc2c0d2b594c5ce3d8
rbbtsn0w <hamiltonsnow@gmail.com>
2026-05-17 13:01:08 +0800

    gh-address-cr 2.4.2

---
+++ Formula/gh-address-cr.rb
@@ -0,0 +1,25 @@
+class GhAddressCr < Formula
+  include Language::Python::Virtualenv
+
+  desc "Deterministic PR review-resolution control plane runtime"
+  homepage "https://github.com/RbBtSn0w/gh-address-cr"
+  url "https://files.pythonhosted.org/packages/9a/5e/5831680b5c84d91a68c8fbc235642114c9a64034c0ef3493c9fde11c822d/gh_address_cr-2.4.2.tar.gz"
+  sha256 "639ba17025a7e7a2a57cb0f924469f5b73c4c6304546d3736fcba71ed21255ac"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/d7/f1/e7a6dd94a8d4a5626c03e4e99c87f241ba9e350cd9e6d75123f992427270/packaging-26.2.tar.gz"
+    sha256 "ff452ff5a3e828ce110190feff1178bb1f2ea2281fa2075aadb987c2fb221661"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gh-address-cr --version")
+    assert_match "\"runtime_version\"", shell_output("#{bin}/gh-address-cr agent manifest")
+  end
+end
